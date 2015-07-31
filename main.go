80f4aaa25f1a3314ebc7a1d72df9d3140ecf375b
fanbingxin <fanbingxin.me@gmail.com>
2015-07-31 14:05:51 +0800

    add server flag

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/russross/blackfriday"
@@ -28,9 +31,26 @@ const tpl = `
 </html>
 `
 
-func main() {
-	flag.Parse()
+var (
+	asServer = flag.Bool("server", false, "run as server")
+	addr     = flag.String("addr", ":8080", "server address")
+	root     = flag.String("root", ".", "server root")
+)
 
+func Markdown(in io.Reader, out io.Writer) error {
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	body := blackfriday.MarkdownCommon(buf)
+	m := map[string]interface{}{
+		"css":  css,
+		"body": string(body),
+	}
+	return template.Must(template.New("markdown").Parse(tpl)).Execute(out, m)
+}
+
+func runcli() {
 	var r io.Reader = os.Stdin
 
 	if flag.NArg() >= 1 {
@@ -42,16 +62,53 @@ func main() {
 		r = f
 	}
 
-	input, err := ioutil.ReadAll(r)
+	err := Markdown(r, os.Stdout)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	body := blackfriday.MarkdownCommon(input)
-
-	m := map[string]interface{}{
-		"css":  css,
-		"body": string(body),
+func serveMarkdown(w http.ResponseWriter, r *http.Request) {
+	var code int = 200
+	var err error
+	defer func() {
+		log.Printf("%s %d %s", r.Method, code, r.URL.Path)
+		if err != nil {
+			w.WriteHeader(code)
+			io.WriteString(w, err.Error())
+		}
+	}()
+	file := filepath.Join(*root, r.URL.Path)
+	if !(strings.HasSuffix(file, ".md") || strings.HasSuffix(file, ".markdown")) {
+		http.FileServer(http.Dir(*root)).ServeHTTP(w, r)
+		return
 	}
-	template.Must(template.New("markdown").Parse(tpl)).Execute(os.Stdout, m)
+	f, err := os.Open(file)
+	if err != nil {
+		if os.IsNotExist(err) {
+			code = 404
+		}
+		return
+	}
+	defer f.Close()
+	err = Markdown(f, w)
+	if err != nil {
+		code = 500
+		return
+	}
+}
+
+func runserver() {
+	log.Printf("Listening on %s, root %s", *addr, *root)
+	http.HandleFunc("/", serveMarkdown)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func main() {
+	flag.Parse()
+	if !*asServer {
+		runcli()
+	} else {
+		runserver()
+	}
 }
